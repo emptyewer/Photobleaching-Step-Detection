3fd2280aad22d9d94d4cdafb7c14a3e97fa22cfe
Venky <venks@andrew.cmu.edu>
2014-12-17 23:28:39 -0600

    License statements (3)

--- photobleaching.m
+++ photobleaching.m
@@ -1,7 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 %     Photobleaching Step Detection
-%     Copyright (C) 2014  Venkatramanan Krishnamani, Rahul Chadda
+%     Copyright (C) 2014  
+%     Venkatramanan Krishnamani, Rahul Chadda & ...
 % 
 %     This program is free software: you can redistribute it and/or modify
 %     it under the terms of the GNU General Public License as published by
